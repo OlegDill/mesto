f90f73569926f7147a8e8034c8eab17a5286bc07
OlegDill <98117609+OlegDill@users.noreply.github.com>
2022-04-22 21:20:19 +0500

    fix адаптив

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,32 @@
-const editButton = document.querySelector('.profile-info__edit-button');
-const popupEdit = document.querySelector('.popup-edit');
-const closeButton = document.querySelector('.popup-edit__form_close-btn');
-
-function popupToogle(popupObject) {
-  popupObject.classList.toggle('hidden');
-}
+const qs = (selector) => document.querySelector(selector);
+const editButton = qs('.profile-info__edit-button');
+const popupEdit = qs('.popup-edit');
+const closeButton = qs('.popup-edit__form_close-btn');
+const bodyOverflow = qs('.body');
 
 editButton.addEventListener('click', function (event) {
   event.preventDefault();
   popupToogle(popupEdit);
+  popupOverflow()
 });
 
 closeButton.addEventListener('click', function (event) {
   const currentPopup = event.target.closest('.popup-edit');
   currentPopup.classList.toggle('hidden');
+  popupOverflow()
 });
 
 popupEdit.addEventListener('click', function (event) {
   if (event.target === event.currentTarget) {
     popupToogle(popupEdit);
+    popupOverflow()
   }
 });
+
+function popupToogle(popupObject) {
+  popupObject.classList.toggle('hidden');
+}
+
+function popupOverflow() {
+  bodyOverflow.classList.toggle('body__overflow');
+}
