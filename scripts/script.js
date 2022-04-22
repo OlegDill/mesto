9f3bca3c8dc85a17b75783787437fddf7a4a39af
OlegDill <98117609+OlegDill@users.noreply.github.com>
2022-04-22 19:46:25 +0500

    add popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,14 @@
-// let container = document.querySelector('.container');
 let editButton = document.querySelector('.profile-info__edit-button');
+let popupEdit = document.querySelector('.popup-edit');
+let closeButton = document.querySelector('.popup-edit__form_close-btn')
 
-function formOpen() {
-  
+function popupOpen() {
+  popupEdit.classList.toggle('hidden');
  }
 
+ function popupClose() {
+  popupEdit.classList.toggle('hidden');
+ }
 
-
-editButton.addEventListener('click', formOpen); 
-
-console.log(formOpen);
+editButton.addEventListener('click', popupOpen); 
+closeButton.addEventListener('click', popupClose); 
