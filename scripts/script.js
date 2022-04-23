1dbfbc89f9dbe000303a0957267e7c1ba7bf50c3
OlegDill <98117609+OlegDill@users.noreply.github.com>
2022-04-23 18:37:37 +0500

    js

--- scripts/script.js
+++ scripts/script.js
@@ -4,11 +4,19 @@ const popupEdit = qs('.popup-edit');
 const closeButton = qs('.popup-edit__form_close-btn');
 const bodyOverflow = qs('.body');
 const like = document.querySelector('.element__item_like');
+const nameInput = qs('.popup-edit__form_input-name');
+const descInput = qs('.popup-edit__form_input-desc');
+const subBtn = qs('.popup-edit__form_sub-btn');
+const nameProfile = qs('.profile-info__caption_title');
+const descProfile = qs('.profile-info__caption_subtitle');
 
 editButton.addEventListener('click', function (event) {
   event.preventDefault();
   popupToogle(popupEdit);
   popupOverflow()
+  nameInput.value = nameProfile.textContent;
+  descInput.value = descProfile.textContent;
+  subBtnDisable()
 });
 
 closeButton.addEventListener('click', function (event) {
@@ -24,13 +32,13 @@ popupEdit.addEventListener('click', function (event) {
   }
 });
 
-console.log(like);
-
 like.addEventListener('click', function (event) {
   console.log('все ок');
   const likeActive = event.target.closest('.element__item_like');
   likeActive.classList.toggle('element__item_like-active');
 });
+
+subBtn.addEventListener('click', formSubmitHandler);
 
 function test() {
   console.log('test');
@@ -42,4 +50,40 @@ function popupToogle(popupObject) {
 
 function popupOverflow() {
   bodyOverflow.classList.toggle('body__overflow');
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    nameProfile.textContent = nameInput.value;
+    descProfile.textContent = descInput.value;
+    popupToogle(popupEdit);
+    popupOverflow()
+}
+
+nameInput.addEventListener('input', subBtnActive);
+descInput.addEventListener('input', subBtnActive);
+
+function subBtnActive() {
+  if (nameInput.value.length > 1 && descInput.value.length > 1) {
+    subBtnEnable();
+  } else {
+    subBtnDisable()
+  }
+  if (nameInput.value === nameProfile.textContent && descInput.value === descProfile.textContent) {
+    subBtnDisable()
+  } else {
+    subBtnEnable();
+  }
+}
+
+function subBtnDisable() {
+  console.log('кнопка выключена');
+  subBtn.setAttribute('disabled', true);
+  subBtn.classList.add('popup-edit__form_sub-btn_disable');
+}
+
+function subBtnEnable() {
+  console.log('кнопка включена');
+  subBtn.removeAttribute('disabled');
+  subBtn.classList.remove('popup-edit__form_sub-btn_disable');
 }
