edcc3d5931c6e7a2fb3dfa95480fda6cb223b2f8
OlegDill <98117609+OlegDill@users.noreply.github.com>
2022-04-23 15:17:38 +0500

    fix js

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ const editButton = qs('.profile-info__edit-button');
 const popupEdit = qs('.popup-edit');
 const closeButton = qs('.popup-edit__form_close-btn');
 const bodyOverflow = qs('.body');
+const like = document.querySelector('.element__item_like');
 
 editButton.addEventListener('click', function (event) {
   event.preventDefault();
@@ -22,6 +23,18 @@ popupEdit.addEventListener('click', function (event) {
     popupOverflow()
   }
 });
+
+console.log(like);
+
+like.addEventListener('click', function (event) {
+  console.log('все ок');
+  const likeActive = event.target.closest('.element__item_like');
+  likeActive.classList.toggle('element__item_like-active');
+});
+
+function test() {
+  console.log('test');
+}
 
 function popupToogle(popupObject) {
   popupObject.classList.toggle('hidden');
