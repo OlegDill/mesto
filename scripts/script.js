2faf3421a48bfbc848b10ce64d2035f3a1293e04
OlegDill <98117609+OlegDill@users.noreply.github.com>
2022-04-30 20:21:02 +0500

    fix popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,21 @@
 const qs = (selector) => document.querySelector(selector);
 const editButton = qs('.profile__edit-button');
 const popupEdit = qs('.popup');
-const closeButton = qs('.popup-edit__close-btn');
-const formElement = qs('.popup-edit'); 
-const nameInput = qs('.popup-edit__input-name');
-const descInput = qs('.popup-edit__input-desc');
+const closeButton = qs('.popup__close-btn');
+const formElement = qs('.popup__form'); 
+const nameInput = qs('.popup__input_name');
+const descInput = qs('.popup__input_desc');
 const nameProfile = qs('.profile__title');
 const descProfile = qs('.profile__subtitle');
 
 function popupEditOpen() {
   nameInput.value = nameProfile.textContent;
   descInput.value = descProfile.textContent;
-  popupEdit.classList.add('hidden');
+  popupEdit.classList.add('popup_hidden', true);
 };
 
 function popupEditClose() {
-  popupEdit.classList.remove('hidden');
+  popupEdit.classList.remove('popup_hidden');
 }
 
 function formSubmitHandler (evt) {
