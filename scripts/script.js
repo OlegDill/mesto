db0ecf8f023dcd12155b09d881c7f27825da33f2
OlegDill <98117609+OlegDill@users.noreply.github.com>
2022-05-05 09:57:17 +0500

    load picture URL

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,41 @@ const nameInput = qs('.popup__input_name');
 const descInput = qs('.popup__input_desc');
 const nameProfile = qs('.profile__title');
 const descProfile = qs('.profile__subtitle');
+const image = document.querySelectorAll('.element__photo');
+const title = document.querySelectorAll('.element__title');
+const initialCards = [
+  {
+    name: 'Карачаевск',
+    link: 'https://i.yapx.ru/R2wZU.jpg'
+  },
+  {
+    name: 'Алтай',
+    link: 'https://i.yapx.ru/R2wZQ.jpg'
+  },
+  {
+    name: 'Якутия',
+    link: 'https://i.yapx.ru/R2wZR.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://i.yapx.ru/R2wZS.jpg'
+  },
+  {
+    name: 'Карелия',
+    link: 'https://i.yapx.ru/R2wZT.jpg'
+  },
+  {
+    name: 'Республика Коми',
+    link: 'https://i.yapx.ru/R2wZV.jpg'
+  }
+];
+
+function ready() {
+  for (let i = 0; i < image.length; i++) {
+    image[i].src = initialCards[i].link;
+    title[i].textContent = initialCards[i].name;
+  }
+}
 
 function popupEditOpen() {
   nameInput.value = nameProfile.textContent;
@@ -25,6 +60,7 @@ function formSubmitHandler (evt) {
   popupEditClose();
 }
 
+document.addEventListener("DOMContentLoaded", ready);
 editButton.addEventListener('click', popupEditOpen);
 closeButton.addEventListener('click', popupEditClose);
 formElement.addEventListener('submit', formSubmitHandler);
