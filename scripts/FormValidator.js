22c5dd4d7e1e358e3a8b6fceeb0597e511d24778
OlegDill <98117609+OlegDill@users.noreply.github.com>
2022-06-23 12:03:32 +0500

    refactoring

---
+++ scripts/FormValidator.js
@@ -0,0 +1,71 @@
+export class FormValidator {
+  constructor(validConfig, formElement) {
+    this._formSelector = validConfig.formSelector
+    this._inputSelector = validConfig.inputSelector;
+    this._submitButtonSelector = validConfig.submitButtonSelector;
+    this._inactiveButtonClass = validConfig.inactiveButtonClass;
+    this._inputErrorClass = validConfig.inputErrorClass;
+    this._errorClass = validConfig.errorClass;
+    this._formElement = formElement;
+    this._buttonSubmit = formElement.querySelector(validConfig.submitButtonSelector);
+  }
+
+  _showInputError(inputElement, errorMessage) {
+    const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add(this._inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(this._errorClass);
+  };
+
+  _hideInputError(inputElement) {
+    const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove(this._inputErrorClass);
+    errorElement.classList.remove(this._errorClass);
+    errorElement.textContent = '';
+  };
+
+  _checkInputValidity(inputElement) {
+    const errorMessage = inputElement.validationMessage;
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement, errorMessage);
+    } else {
+      this._hideInputError(inputElement);
+    }
+  };
+
+  _hasInvalidInput() {
+    return this._inputList.some(inputElement => {
+      return !inputElement.validity.valid;
+    });
+  };
+
+  _toogleButtonState() {
+    if (this._hasInvalidInput(this._inputList)) {
+      this._buttonSubmit.disabled = true;
+      this._buttonSubmit.classList.add(this._inactiveButtonClass);
+    } else {
+      this._buttonSubmit.disabled = false;
+      this._buttonSubmit.classList.remove(this._inactiveButtonClass);
+    }
+  };
+
+  _setEventListeners() {
+    this._inputList = Array.from(this._formElement.querySelectorAll(this._inputSelector));
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', () => {
+        this._checkInputValidity(inputElement);
+        this._toogleButtonState();
+      });
+    });
+  };
+  
+  deleteError() {
+    this._inputList.forEach(inputElement => {
+      this._hideInputError(inputElement);
+    })
+  };
+
+  enableValidation() {
+    this._setEventListeners();
+  };
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,6 @@
+import { Card } from "./Card.js";
+import { FormValidator } from "./FormValidator.js";
+
 const profileButtonEdit = document.querySelector('.profile__edit-button');
 const popupEdit = document.querySelector('.popup-edit');
 const popupEditForm = document.querySelector('.popup-edit__form');
@@ -12,12 +15,55 @@ const popupAddSubBtn = document.querySelector('.popup-add__sub-btn');
 const elementNew = document.querySelector('.elements');
 const popupAddInputName = document.querySelector('.popup-add__input-name');
 const popupAddInputDesc = document.querySelector('.popup-add__input-desc');
-const popupPhoto = document.querySelector('.popup-photo');
-const popupPhotoImage = document.querySelector('.popup-photo__image');
-const popupPhotoSubtitle = document.querySelector('.popup-photo__subtitle');
-const template = document.querySelector('.temp');
+export const popupPhoto = document.querySelector('.popup-photo');
+export const popupPhotoImage = document.querySelector('.popup-photo__image');
+export const popupPhotoSubtitle = document.querySelector('.popup-photo__subtitle');
 const body = document.querySelector('.body');
 const popups = document.querySelectorAll('.popup')
+
+const initialCards = [
+  {
+    name: 'Республика Коми',
+    link: 'https://www.russiadiscovery.ru/upload/files/files/Puteshestvie-na-Ural%281%29.jpg'
+  },
+  {
+    name: 'Алтай',
+    link: 'https://www.russiadiscovery.ru/upload/files/files/%D0%90%D0%BB%D1%82%D0%B0%D0%B8%CC%86-%281%29%281%29.jpg'
+  },
+  {
+    name: 'Якутия',
+    link: 'https://www.russiadiscovery.ru/upload/files/files/%D0%9B%D0%B5%D0%BD%D1%81%D0%BA%D0%B8%D0%B5_%D1%81%D1%82%D0%BE%D0%BB%D0%B1%D1%8B.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://www.russiadiscovery.ru/upload/files/files/Trekking-na-Kamchatke%281%29.jpg'
+  },
+  {
+    name: 'Карелия',
+    link: 'https://www.russiadiscovery.ru/upload/files/files/%D0%A0%D1%83%D1%81%D0%BA%D0%B5%D0%B0%D0%BB%D0%B0.jpg'
+  },
+  {
+    name: 'Карачаевск',
+    link: 'https://i.yapx.ru/R2zrO.png'
+  }
+];
+
+const validConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__sub-btn',
+  inactiveButtonClass: 'popup__sub-btn_disabled',
+  inputErrorClass: 'popup__input_type-error',
+  errorClass: 'popup__error_visible'
+};
+
+const formValidators = {};
+
+Array.from(document.forms).forEach(formElement => {
+  formValidators[formElement.name] = new FormValidator(validConfig, formElement);
+  formValidators[formElement.name].enableValidation();
+});
+
 // обработчики клика по оверлею и крестику
 popups.forEach((popup) => {
   popup.addEventListener('mousedown', (evt) => {
@@ -29,34 +75,39 @@ popups.forEach((popup) => {
       }
   })
 });
-// функция создания новой карточки
-const getItemElement = (title, photo) => {
-  const newItemElement = template.content.cloneNode(true);
-  const newItemPhoto = newItemElement.querySelector('.element__photo');
-  const newItemTitle = newItemElement.querySelector('.element__title');
-  newItemTitle.textContent = title;
-  newItemPhoto.src = photo;
-  newItemPhoto.alt = 'Фото ' + title;
-  const elementButtonLike = newItemElement.querySelector('.element__like');
-  const elementButtonDelete = newItemElement.querySelector('.element__delete');
-  elementButtonLike.addEventListener('click', likeCard);
-  elementButtonDelete.addEventListener('click', deleteCard);
-  newItemPhoto.addEventListener('click', () => openCard(title, photo));
-  return newItemElement;
-};
+// // функция создания новой карточки
+// const getItemElement = (title, photo) => {
+//   const newItemElement = template.content.cloneNode(true);
+//   const newItemPhoto = newItemElement.querySelector('.element__photo');
+//   const newItemTitle = newItemElement.querySelector('.element__title');
+//   newItemTitle.textContent = title;
+//   newItemPhoto.src = photo;
+//   newItemPhoto.alt = 'Фото ' + title;
+//   const elementButtonLike = newItemElement.querySelector('.element__like');
+//   const elementButtonDelete = newItemElement.querySelector('.element__delete');
+//   elementButtonLike.addEventListener('click', likeCard);
+//   elementButtonDelete.addEventListener('click', deleteCard);
+//   newItemPhoto.addEventListener('click', () => openCard(title, photo));
+//   return newItemElement;
+// };
 // функция добавления карточек
 const renderItem = (wrap, title, photo) => {
-  wrap.prepend(getItemElement(title, photo));
+  const card = new Card(title, photo, '.temp');
+  wrap.prepend(card.generateCard());
 };
-// добавления стартовых карточек
-initialCards.forEach((title) => {
-  renderItem(elementNew, title.name, title.link)
+// добавление стартовых карточек
+initialCards.forEach((name, link) => {
+  const card = new Card(name, link, '.element');
+  const cardElement = card.generateCard();
+  document.querySelector('.elements').prepend(cardElement);
+  // renderItem(elementNew, title.name, title.link)
 });
 // функция открытия popup-edit
 function openPopupEdit() {
   popupEditInputName.value = profileTitle.textContent;
   popupEditInputDesc.value = profileSubtitle.textContent;
   openPopup(popupEdit);
+  formValidators[popupEditForm.name].deleteError();
 };
 // функция сохранения данных с popup-edit
 function handleFormEditSubmit() {
@@ -73,22 +124,22 @@ function handleFormAddSubmit(event) {
   popupAddSubBtn.classList.add('popup__sub-btn_disabled');   
 };
 // функция лайков карточкам
-function likeCard(evt) { 
-  evt.target.classList.toggle('element__like_active');
-};
+// function likeCard(evt) { 
+//   evt.target.classList.toggle('element__like_active');
+// };
 // функция удаления карточек
-function deleteCard(evt) { 
-  evt.target.closest('.element').remove();
-};
+// function deleteCard(evt) { 
+//   evt.target.closest('.element').remove();
+// };
 // функция открытия popup-photo
-function openCard(title, photo) {
-  popupPhotoImage.src = photo;
-  popupPhotoImage.alt = 'Фото ' + title;
-  popupPhotoSubtitle.textContent = title;
-  openPopup(popupPhoto);
-};
+// function openCard(title, photo) {
+//   popupPhotoImage.src = photo;
+//   popupPhotoImage.alt = 'Фото ' + title;
+//   popupPhotoSubtitle.textContent = title;
+//   openPopup(popupPhoto);
+// };
 // функция открытия popup
-function openPopup(popup) {
+export function openPopup(popup) {
   popup.classList.add('popup_opened', true);
   togglePopupOverflow();
   document.addEventListener('keydown', pressEscapeKey);
@@ -115,3 +166,5 @@ profileButtonEdit.addEventListener('click', openPopupEdit);
 buttonAdd.addEventListener('click', () => openPopup(popupAdd));
 popupEditForm.addEventListener('submit', handleFormEditSubmit);
 popupAddForm.addEventListener('submit', handleFormAddSubmit);
+
+// enableValidation(config);
